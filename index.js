3d551255b03da384c7f92361613358728755116f
Fabrzio Lasaponara <fabrzio.lasaponara@hotmail.it>
2019-06-28 12:09:18 +0200

    Add connection to database

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose')
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+mongoose.connect("mongodb+srv://dbUser:Matematica93@cluster0-gdac1.azure.mongodb.net/subscribers?retryWrites=true&w=majority" , { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error',(error) => console.error(error))
+db.once('open',() => console.error('Connected to Database'))
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
