c20d809eaa2a8b0d0024b31bfbc5d9a133074360
Fabrzio Lasaponara <fabrzio.lasaponara@hotmail.it>
2019-06-28 23:10:51 +0200

    add cors methods

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
