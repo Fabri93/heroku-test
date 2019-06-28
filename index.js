48ef98b8a5feb8ab329f3103cbaeeb2d23494e88
Fabrzio Lasaponara <fabrzio.lasaponara@hotmail.it>
2019-06-28 13:57:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes/api');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+//connect to the database
 mongoose.connect("mongodb+srv://dbUser:Matematica93@cluster0-gdac1.azure.mongodb.net/subscribers?retryWrites=true&w=majority" , { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error',(error) => console.error(error))
@@ -22,10 +26,14 @@ app.use((req, res, next) => {
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
-//Create root route
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
-});
+//Make possible parsing a json response
+app.use(bodyParser.json());
+
+//Create root route logging the error
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
